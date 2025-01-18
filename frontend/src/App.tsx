5318cfbe08a408ae9991bffe3741552dac13a85e
AmanYadav1419 <amanyadav16544@gmail.com>
2025-01-18 13:28:23 +0530

    done setup of shadcn, solved error of tsconfig.node.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <h1 className="text-red-400 text-4xl">Hello from EchoChain!!</h1>
+      <Button variant={"outline"}>This is ShadCN Button !</Button>
     </>
   )
 }
