351438fc8dcd0c6d2a98973ddc5ef61dd519f079
unknown <amanyadav16544@gmail.com>
2025-01-18 20:41:09 +0530

    added clerk as authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import { Button } from "./components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1 className="text-red-400 text-4xl">Hello from EchoChain!!</h1>
-      <Button variant={"outline"}>This is ShadCN Button !</Button>
-    </>
-  )
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 }
-
-export default App
