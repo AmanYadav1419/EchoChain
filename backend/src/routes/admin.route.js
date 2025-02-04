6c61d42365e764f037ef4ea982d780d3ad5a3799
AmanYadav1419 <amanyadav16544@gmail.com>
2025-02-04 14:27:21 +0530

    created deleteSong function in admin controller

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createSong } from "../controller/admin.controller.js";
+import { createSong, deleteSong } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
@@ -7,5 +7,8 @@ const router = Router();
 // at admin route it wil call the getAdmin function which is on admin.controller.js
 // for that before that call the middleware functions
 router.post("/songs", protectRoute, requireAdmin, createSong);
+
+// for delete song route and function with having middleware
+router.delete("songs/:id", protectRoute, requireAdmin, deleteSong)
 
 export default router;
