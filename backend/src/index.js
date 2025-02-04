0cbb29f44e01a9966f25a4ad6a676a00c01e98e9
unknown <amanyadav16544@gmail.com>
2025-02-04 11:21:57 +0530

    setup of cloudinary and express-fileupload

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,9 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import {clerkMiddleware} from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -16,6 +18,7 @@ import statRoutes from "./routes/stat.route.js";
 // after doing / caling this function we can run env file values
 dotenv.config();
 
+const __dirname = path.resolve();
 // call the express function and save it to in variable app
 const app = express();
 const PORT = process.env.PORT;
@@ -25,7 +28,23 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 // it will add auth to req object
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
+
+// this is act as an middleware for uploading file
+app.use(
+  fileUpload({
+    // use temp files true
+    useTempfiles: true,
+    // it will create the temp folder to store the temporary data i.e audio and image
+    // upto the audio and image data is get uploaded to cloudinary
+    tempFileDir: path.join(__dirname, "tmp"),
+    // and if the tmp folder doesn't exists it will create that, like for the first time
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB max file size
+    },
+  })
+);
 
 // if user hit the api route , then run userRoutes function, present in routes folder
 app.use("/api/users", userRoutes);
