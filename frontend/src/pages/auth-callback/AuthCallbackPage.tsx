239833f8dbf7ed2cb9d462e01faf34d3eed23e38
AmanYadav1419 <amanyadav16544@gmail.com>
2025-02-08 14:33:02 +0530

    added & solved card component issue

--- frontend/src/pages/auth-callback/AuthCallbackPage.tsx
+++ frontend/src/pages/auth-callback/AuthCallbackPage.tsx
@@ -1,8 +1,12 @@
+import { Card } from "@/components/ui/card";
+
 const AuthCallbackPage = () => {
     return (
 
         <div className="h-screen w-full bg-black flex items-centre justify-centre">
-            
+            <Card>
+                {/* write your code from 2:28:20 */}
+            </Card>
         </div>
     )
 };
